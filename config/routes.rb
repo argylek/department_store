170b0c70a3e771e24ac4951e847db816c9f06dbb
Scott Key <scottdkey@gmail.com>
2020-02-20 14:51:55 -0700

    department created and stable.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # get 'departments/index'
+  # get 'departments/show'
+  # get 'departments/edit'
+  # get 'departments/new'
+
+  resources :departments
+  root 'departments#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
