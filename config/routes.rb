89cff0577b2563834b36d9fb28f17ab8d20637ed
Scott Key <scottdkey@gmail.com>
2020-02-20 16:27:26 -0700

    Created new model and controller for departments/items - We've migrated

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
 
   resources :departments
   root 'departments#index'
+
+  resources :departments do
+    resources :items
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
